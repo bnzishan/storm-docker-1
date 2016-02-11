553096894f416ab45da35c15000697e4b27a16a8
Markus Mayer <widemeadows@gmail.com>
2016-02-11 23:15:14 +0100

    fixed supervisor port

--- storm/bin/start-supervisor.sh
+++ storm/bin/start-supervisor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ZOOKEEPER_ADDRESS=${ZOOKEEPER_ADDRESS:=$ZK_PORT_2181_TCP_ADDR}
+ZOOKEEPER_PORT=${ZOOKEEPER_PORT:=2181}
 NIMBUS_ADDRESS=${NIMBUS_ADDRESS:=$NIMBUS_PORT_6627_TCP_ADDR}
 SUPERVISOR_ADDRESS=${SUPERVISOR_ADDRESS:=`hostname -i`}
 
@@ -16,6 +17,7 @@ fi
 
 # update the configuration file
 sed -i -e "s/%zookeeper%/$ZOOKEEPER_ADDRESS/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%zookeeper.port%/$ZOOKEEPER_PORT/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_ADDRESS/g" $STORM_HOME/conf/storm.yaml
 echo "storm.local.hostname: $SUPERVISOR_ADDRESS" >> $STORM_HOME/conf/storm.yaml
 
